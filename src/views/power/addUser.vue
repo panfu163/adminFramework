8bddca5c6599650aa01895bbdd26591616f8fef9
1602858720@qq.com <1602858720@qq.com>
2020-08-24 17:16:36 +0800

    增加数据校验功能

--- src/views/power/addUser.vue
+++ src/views/power/addUser.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="content">
     <h4>{{this.$route.meta.title}}</h4>
-    <el-form ref="formId" :inline="true" :model="form" label-width="100px">
+    <el-form ref="formName" :inline="true" :model="ruleForm" :rules="rules" label-width="100px">
       <el-form-item label="用户账号:" prop="name">
-        <el-input v-model="form.name" minlength="6"  placeholder="必填"></el-input>
+        <el-input v-model="ruleForm.name" minlength="6"  placeholder="必填"></el-input>
       </el-form-item>
       <el-row>
         <el-form-item label="账号密码:" prop="password">
-          <el-input v-model="form.password" minlength="6"  placeholder="必填" show-password></el-input>
+          <el-input v-model="ruleForm.password" minlength="6"  placeholder="必填" show-password></el-input>
         </el-form-item>
       </el-row>
       <el-row>
         <el-form-item label="联系手机号:" prop="phone">
-          <el-input v-model="form.phone" type="number" placeholder="必填" minlength="11" maxlength="11"></el-input>
+          <el-input v-model="ruleForm.phone" type="number" placeholder="必填" minlength="11" maxlength="11"></el-input>
         </el-form-item>
       </el-row>
       <el-row>
         <el-form-item label="联系邮箱:"  prop="mailbox">
-          <el-input v-model="form.mailbox"  type="email"  placeholder="必填"></el-input>
+          <el-input v-model="ruleForm.mailbox"  type="email"  placeholder="必填"></el-input>
         </el-form-item>
       </el-row>
       <el-row>
       <el-form-item label="真实姓名:" prop="fullName">
-        <el-input v-model="form.fullName"  placeholder="必填"></el-input>
+        <el-input v-model="ruleForm.fullName"  placeholder="必填"></el-input>
       </el-form-item>
       </el-row>
       <el-row>
-        <el-form-item label="所在部门:">
-          <el-select v-model="form.region" placeholder="请选择部门">
+        <el-form-item label="所在部门:" prop="region">
+          <el-select v-model="ruleForm.region" placeholder="请选择部门">
             <el-option label="总经办" value="houqing"></el-option>
             <el-option label="财务部" value="chawu"></el-option>
             <el-option label="技术部" value="jinshu"></el-option>
@@ -79,8 +79,8 @@
         </el-row>
       </el-row>
     </el-form>
-    <el-button class="btn" type="primary" @click="onSubmit">确定</el-button>
-    <el-button class="btn" @click="onReset">重置</el-button>
+    <el-button class="btn" type="primary" @click="onSubmit('formName')">确定</el-button>
+    <el-button class="btn" @click="onReset('formName')">重置</el-button>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -89,7 +89,6 @@
     background: #fff;
     box-sizing: border-box;
     width:100%;
-    overflow:hidden;
     h4{
       border-bottom: 1px solid #eee;
       padding-bottom:20px;
@@ -110,8 +109,6 @@
     }
     .user-group{
       width:100%;
-      height:280px;
-      overflow-y:auto;
     }
   }
 </style>
@@ -119,13 +116,36 @@
   export default {
     data() {
       return {
-        form: {
+        ruleForm: {
           name: "",
           password:"",
           phone: "",
           fullName: "",
           desc: "",
           mailbox:""
+        },
+        rules: {
+          name: [
+            { required: true, message: '请输入用户账号名称', trigger: 'blur' },
+            { min:6, max:20, message: '长度在6到20个字符', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入用户账号密码', trigger: 'blur' },
+            { min:6, max:20, message: '长度在6到20个字符', trigger: 'blur' }
+          ],
+          phone: [
+            { required: true, message: '请输入联系电话', trigger: 'blur' },
+            { min:11, max:11, message: '长度为11个字符', trigger: 'blur' }
+          ],
+          fullName: [
+            { required: true, message: '请输入您的姓名', trigger: 'blur' }
+          ],
+          mailbox: [
+            { required: true, message: '请输入您的邮箱地址', trigger: 'blur' }
+          ],
+          region: [
+            { required: true, message: '请选择所在的部门', trigger: 'change' }
+          ],
         },
         data: [{
           id: 1,
@@ -190,12 +210,20 @@
     },
     methods: {
       //确定
-      onSubmit() {
+      onSubmit(formName) {
         console.log("submit!");
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
       },
       //重置
-      onReset(){
-        this.$refs.formId.resetFields();
+      onReset(formName){
+        this.$refs[formName].resetFields();
         this.$refs.newTopRightsTree.setCheckedKeys([])//树形重置
       },
       loadNode(node, resolve) {
@@ -233,6 +261,7 @@
           resolve(data);
         }, 500);
       },
+      handleCheckChange(){}
     },
     mounted() {
 
